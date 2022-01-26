c0adb8e0f75906dd6771a142d8fe82eb8888d639
Mustafa Tortuk <mustafat237@hotmail.com>
2022-01-26 17:18:51 +0300

    Add files via upload

---
+++ NotOrt/NotOrt.java
@@ -0,0 +1,31 @@
+package NotOrt;
+import java.util.Scanner;
+public class NotOrt{
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+
+        int matematik,fizik,kimya,turkce,tarih,muzik;
+
+        System.out.print("Ogrencinin matematik notunu giriniz :");
+        matematik = input.nextInt();
+
+        System.out.print("Ogrencinin fizik notunu giriniz :");
+        fizik = input.nextInt();
+
+        System.out.print("Ogrencinin kimya notunu giriniz :");
+        kimya = input.nextInt();
+
+        System.out.print("Ogrencinin turkce notunu giriniz :");
+        turkce = input.nextInt();
+
+        System.out.print("Ogrencinin tarih notunu giriniz :");
+        tarih = input.nextInt();
+
+        System.out.print("Ogrencinin muzik notunu giriniz :");
+        muzik = input.nextInt();
+
+        double ort = (matematik+fizik+kimya+turkce+tarih+muzik)/6.0;
+
+        System.out.println(ort>=60?"Gecti!":"Kaldi!");
+    }
+}
