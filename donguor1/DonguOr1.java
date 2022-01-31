10eced7d61b862d9cb92962c48d374b1f636d07f
Mustafa Tortuk <mustafat237@hotmail.com>
2022-01-31 12:26:50 +0300

    Add files via upload

---
+++ donguor1/DonguOr1.java
@@ -0,0 +1,25 @@
+package donguor1;
+import java.util.Scanner;
+public class DonguOr1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int toplam = 0;
+        int sayac =0;
+        System.out.print("Bir sayi giriniz : ");
+        n = sc.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            if (i%12==0) {
+                System.out.print(i+" ");
+                toplam +=i;
+                sayac++;
+            }
+        }
+        
+        System.out.println("Ortalama : "+(double)(toplam/sayac));
+
+        sc.close();
+    }
+    
+}
