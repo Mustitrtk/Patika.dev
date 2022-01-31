6e42ef41d79767d2fb9af4f6e20941e4905aa9aa
Mustafa Tortuk <mustafat237@hotmail.com>
2022-01-31 14:28:12 +0300

    Add files via upload

---
+++ basamaktoplam/BasamakToplam.java
@@ -0,0 +1,19 @@
+package basamaktoplam;
+import java.util.Scanner;
+public class BasamakToplam {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int toplam =0;
+        System.out.print("Bir sayi giriniz : ");
+        n = sc.nextInt();
+        
+        while(n>0){
+            toplam += (n%10);
+            n = n/10;
+        }
+        
+        System.out.println("Basamaklar toplamı : "+toplam);
+    }
+    
+}
