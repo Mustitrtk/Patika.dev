2b1d66dcef2666045f748740003c5ce65b61b609
Mustafa Tortuk <mustafat237@hotmail.com>
2022-02-01 13:58:10 +0300

    Add files via upload

---
+++ polindromiksayi/PolindromikSayi.java
@@ -0,0 +1,32 @@
+package polindromiksayi;
+import java.util.Scanner;
+public class PolindromikSayi {
+    
+    public static boolean isPolindromic(int x){
+        int toplam =0,temp = x,basamak;
+        while(temp!=0){
+            basamak = temp%10;
+            toplam = toplam*10+basamak;
+            temp/=10;
+        }
+        
+        if (x == toplam) {
+            return true;
+        }
+        else
+            return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Bir sayi giriniz : ");
+        int n = sc.nextInt();
+        
+        if (isPolindromic(n)==true) {
+            System.out.println("Sayi polindromiktir.");
+        }
+        else System.out.println("Sayi polindromik degildir.");
+        
+        sc.close();
+    }
+    
+}
