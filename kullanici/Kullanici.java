437a7f3e6227b764df1b54880f731bd2170c182d
Mustafa Tortuk <mustafat237@hotmail.com>
2022-02-03 13:08:25 +0300

    Add files via upload

---
+++ kullanici/Kullanici.java
@@ -0,0 +1,47 @@
+package kullanici;
+import java.util.Scanner;
+public class Kullanici {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String UserName="java101",Password="PatikaDev";
+        
+        System.out.print("Kullanici adini giriniz : ");
+        String admin = sc.next();
+        System.out.print("Sifre giriniz : ");
+        String adminPassword = sc.next();
+        
+        if (admin.equals(UserName) && adminPassword.equals(Password)){
+            System.out.println("Giris basarili.");
+        }
+        if (!adminPassword.equals(Password)) {
+            System.out.println("Sifre yanlis.");
+            System.out.print("Sifreyi degistirmek ister misiniz ? e/h");
+            char secim = sc.next().charAt(0);
+            switch(secim)
+            {
+                case 'e':
+                    System.out.print("Yeni sifre giriniz : ");
+                    adminPassword=sc.next();
+                    if (!adminPassword.equals(Password)) {
+                        Password = adminPassword;
+                        System.out.println("Sifre basari ile degistirildi.");
+                    }
+                    else System.out.println("Eski sifre yenisi ile ayni olamaz.");
+                    
+                    break;
+                case 'h':
+                    break;
+                
+                default:
+                    System.out.println("Hatali kullanici girisi.");
+                    break;
+            }
+        }
+        
+        else System.out.println("Kullanici adi hatali girildi.");
+        
+        sc.close();
+    }
+    
+}
