fdc3ca35a47783518f380c8fc8d93528c85c43d0
Mustafa Tortuk <mustafat237@hotmail.com>
2022-02-05 13:56:26 +0300

    Add files via upload

---
+++ Fibonacci/Fibonacci.java
@@ -0,0 +1,12 @@
+package Fibonacci;
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n=0,n2=1,temp;
+        for(int i =0;i<8;i++){
+            temp = n+n2;
+            System.out.print(temp+" ");
+            n=n2;
+            n2=temp;
+        }
+    }
+}
